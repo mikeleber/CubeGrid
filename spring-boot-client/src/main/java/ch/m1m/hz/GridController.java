340c158d1c87403bc47e12d78e955e0f03182ade
mmm <mmm7@bluewin.ch>
2021-11-02 15:49:48 +0100

    Add colored background to the index template

--- spring-boot-client/src/main/java/ch/m1m/hz/GridController.java
+++ spring-boot-client/src/main/java/ch/m1m/hz/GridController.java
@@ -1,5 +1,6 @@
 package ch.m1m.hz;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,17 +8,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GridController {
 
+    @Value("${server.port}")
+    String portAsString;
+
     @GetMapping("/")
     public String showUserListIndex(Model model) {
+        return modelIndex(model);
+    }
+
+    @GetMapping("/index")
+    public String showUserList(Model model) {
+        return modelIndex(model);
+    }
+
+    private String modelIndex(Model model) {
+
+        model.addAttribute("headerBackground", getHeaderBackground());
+
         model.addAttribute("user", "totoUser");
         model.addAttribute("myuser", "MikeMagic");
         return "index";
     }
 
-    @GetMapping("/index")
-    public String showUserList(Model model) {
-        model.addAttribute("user", "totoUser");
-        model.addAttribute("myuser", "MikeMagic");
-        return "index";
+    private String getHeaderBackground() {
+        String valueHeaderBackground = "blue";
+        if ("8095".equals(portAsString)) {
+            valueHeaderBackground = "green";
+        }
+        return valueHeaderBackground;
     }
 }
